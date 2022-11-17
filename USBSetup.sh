2fb3286d6dfdce58a38e8c9c7e4f51864c14d549
Gavin Wilson <gavinswilson@gmail.com>
2022-11-17 17:54:58 -0600

    simple but effective version

--- USBSetup.sh
+++ USBSetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd
-
+echo "Plug in the USB hub to be used: (you have 5 seconds)"
 timeout -s SIGTERM 5s udevadm monitor --subsystem-match="usb" -k > add
 
 USBstring=`more add | grep add | grep :1.0 | head -1`
@@ -16,14 +16,14 @@ regstr='^.*[\/](.+?)\:'
 
 USBstring=${BASH_REMATCH[1]}
 
-echo "how many ports?"
+echo "how many ports are on the hub?"
 read numports
 
-echo "starting port"
-read startport
+echo "starting cell number"
+read startcell
 rm /etc/udev/rules.d/99-usb-serial.rules
 
-i=0
+i=$startcell
 j=1
 while [[ $i -lt $numports ]]; do
   echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j:1.0\", SYMLINK+=\"Cell$i\"" >>/etc/udev/rules.d/99-usb-serial.rules
@@ -32,3 +32,4 @@ while [[ $i -lt $numports ]]; do
 done
 echo "Done!"
 more /etc/udev/rules.d/99-usb-serial.rules
+echo "Rules Created, please unplug and replug the USB into the same port"
