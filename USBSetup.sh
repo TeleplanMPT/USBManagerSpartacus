cb7bb50a14963ba193eaa6bec861a2f5967aaa1a
Gavin Wilson <gavinswilson@gmail.com>
2022-11-29 18:28:47 -0600

    setup finalised

--- USBSetup.sh
+++ USBSetup.sh
@@ -20,7 +20,7 @@ USBstring=${BASH_REMATCH[1]}
 echo "What is the make of the hub?"
 read hubmake
 
-if [ $hubmake != "myhub" ]
+if [ $hubmake != "acasis" ]
 then
   echo "hub not supported"
   exit
@@ -35,34 +35,34 @@ rm /etc/udev/rules.d/99-usb-serial.rules
 
 if [ $numports = 10 ]
 then
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.1.1:1.0\", SYMLINK+=\"Cell0\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.1.2:1.0\", SYMLINK+=\"Cell1\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.1.3:1.0\", SYMLINK+=\"Cell2\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.1.4:1.0\", SYMLINK+=\"Cell3\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.2.1:1.0\", SYMLINK+=\"Cell4\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.2.2:1.0\", SYMLINK+=\"Cell5\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.2.3:1.0\", SYMLINK+=\"Cell6\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.2.4:1.0\", SYMLINK+=\"Cell7\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.3:1.0\", SYMLINK+=\"Cell8\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.4:1.0\", SYMLINK+=\"Cell9\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}1.1:1.0\", SYMLINK+=\"Cell0\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}1.2:1.0\", SYMLINK+=\"Cell1\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}1.3:1.0\", SYMLINK+=\"Cell2\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}1.4:1.0\", SYMLINK+=\"Cell3\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}2.1:1.0\", SYMLINK+=\"Cell4\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}2.2:1.0\", SYMLINK+=\"Cell5\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}2.3:1.0\", SYMLINK+=\"Cell6\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}2.4:1.0\", SYMLINK+=\"Cell7\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}3:1.0\", SYMLINK+=\"Cell8\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}4:1.0\", SYMLINK+=\"Cell9\"" >>/etc/udev/rules.d/99-usb-serial.rules
 elif [ $numports = 16 ]
 then
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.2.1:1.0\", SYMLINK+=\"Cell0\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.2.2:1.0\", SYMLINK+=\"Cell1\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.2.3:1.0\", SYMLINK+=\"Cell2\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.2.4:1.0\", SYMLINK+=\"Cell3\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.3.1:1.0\", SYMLINK+=\"Cell4\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.3.2:1.0\", SYMLINK+=\"Cell5\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.3.3:1.0\", SYMLINK+=\"Cell6\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.3.4:1.0\", SYMLINK+=\"Cell7\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.4.1:1.0\", SYMLINK+=\"Cell8\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.4.2:1.0\", SYMLINK+=\"Cell9\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.4.3:1.0\", SYMLINK+=\"Cell10\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.4.4:1.0\", SYMLINK+=\"Cell11\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.1.1:1.0\", SYMLINK+=\"Cell12\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.1.2:1.0\", SYMLINK+=\"Cell13\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.1.3:1.0\", SYMLINK+=\"Cell14\"" >>/etc/udev/rules.d/99-usb-serial.rules
-  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j.1.4:1.0\", SYMLINK+=\"Cell15\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}2.1:1.0\", SYMLINK+=\"Cell0\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}2.2:1.0\", SYMLINK+=\"Cell1\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}2.3:1.0\", SYMLINK+=\"Cell2\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}2.4:1.0\", SYMLINK+=\"Cell3\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}3.1:1.0\", SYMLINK+=\"Cell4\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}3.2:1.0\", SYMLINK+=\"Cell5\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}3.3:1.0\", SYMLINK+=\"Cell6\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}3.4:1.0\", SYMLINK+=\"Cell7\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}4.1:1.0\", SYMLINK+=\"Cell8\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}4.2:1.0\", SYMLINK+=\"Cell9\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}4.3:1.0\", SYMLINK+=\"Cell10\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}4.4:1.0\", SYMLINK+=\"Cell11\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}1.1:1.0\", SYMLINK+=\"Cell12\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}1.2:1.0\", SYMLINK+=\"Cell13\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}1.3:1.0\", SYMLINK+=\"Cell14\"" >>/etc/udev/rules.d/99-usb-serial.rules
+  echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.${j}1.4:1.0\", SYMLINK+=\"Cell15\"" >>/etc/udev/rules.d/99-usb-serial.rules
 else
   i=$startcell
   j=1
@@ -74,7 +74,7 @@ else
 fi
 
 
-echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j:1.0\", SYMLINK+=\"Cell$i\"" >>/etc/udev/rules.d/99-usb-serial.rules
+#echo "SUBSYSTEM==\"tty\", KERNELS==\"$USBstring.$j:1.0\", SYMLINK+=\"Cell$i\"" >>/etc/udev/rules.d/99-usb-serial.rules
 
 echo "Done!"
 more /etc/udev/rules.d/99-usb-serial.rules
